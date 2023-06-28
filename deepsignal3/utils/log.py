842e56297e02ecffcbb7412c974ba687d9651165
centralSouth <3529967102@qq.com>
2023-06-28 21:47:06 +0800

    yield

---
+++ deepsignal3/utils/log.py
@@ -0,0 +1,34 @@
+import sys
+import logging
+
+
+CONSOLE = logging.StreamHandler()
+CONSOLE.setLevel(logging.INFO)
+CONSOLE.setFormatter('%(asctime)s - %(filename)s - line:%(lineno)d - %(levelname)s - %(message)s -%(process)s')
+ROOT_LOGGER = logging.getLogger("Remora")
+ROOT_LOGGER.setLevel(logging.DEBUG)
+ROOT_LOGGER.addHandler(CONSOLE)
+
+
+def init_logger(log_fn=None, quiet=False):
+    """Prepare logging output.
+
+    Args:
+        log_fn (str): Path to logging output file. All logging messages,
+            including debug level, will be output to this file.
+        quiet (bool): Set console logging level to warning. Default info.
+    """
+    log_fp = None
+    if log_fn is not None:
+        log_fp = logging.FileHandler(log_fn, mode="a")
+        log_fp.setLevel(logging.DEBUG)
+        log_fp.setFormatter('%(asctime)s - %(filename)s - line:%(lineno)d - %(levelname)s - %(message)s -%(process)s')
+    if log_fp is not None:
+        ROOT_LOGGER.addHandler(log_fp)
+    if quiet:
+        CONSOLE.setLevel(logging.WARNING)
+    logging.getLogger("Remora").debug(f'Command: """{" ".join(sys.argv)}"""')
+
+
+def get_logger(module_name="deepsignal"):
+    return logging.getLogger(module_name)
---
+++ deepsignal3/utils/utils.py
@@ -0,0 +1,19 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log-file', type=str, default="/home/xiaoyf/methylation/deepsignal/log/test.log",
+                        help='log store address.')
+    parser.add_argument('--output-file', type=str, default="/home/xiaoyf/methylation/deepsignal/log/data.npy",
+                        help='feature file store address.')
+    parser.add_argument('--pod5-file', type=str, default='/homeb/xiaoyf/data/HG002/example/pod5/output.pod5',
+                        help='pod5 file store address.')
+    parser.add_argument('--bam-file', type=str, default='/homeb/xiaoyf/data/HG002/example/bam/has_moves.bam',
+                        help='bam file store address.')
+    parser.add_argument('--nproc', type=int, default=4,
+                        help='number of processes in extract features.')
+    parser.add_argument('--batch-size', type=int, default=200,
+                        help='size of batch in extract features.')
+    parser.add_argument('--window-size', type=int, default=21,
+                        help='size of window in extract neighbor features for target base.')
+    return parser.parse_args()
